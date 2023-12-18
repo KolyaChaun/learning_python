b599849701bd4179fd78c9111dc3b1690c5a5733
Mykola <kolia.chaun@icloud.com>
2023-12-18 14:23:35 +0200

    commit for ninth lesson functions

---
+++ constants.py
@@ -0,0 +1,5 @@
+MSG_USER_INPUT = "Enter the number in centimeters (only digits allowed):"
+MSG_NOT_NUMERIC_INPUT = "You have entered not a number. Try again."
+MSG_USER_INPUT_NUMBERS = "Enter numbers separated by spaces (only digits allowed):"
+MSG_USER_INPUT_MORTGAGE = "Enter your total mortgage amount (only digits allowed):"
+MSG_USER_INPUT_INCOME = "Enter your monthly income (only digits allowed):"
