f86f915cf7922bb54cc0e857c711557abf2be7c5
Mykola <kolia.chaun@icloud.com>
2023-12-29 19:24:44 +0200

    commit for twelfth lesson oop(fix)

--- twelfth_lesson_oop.py
+++ twelfth_lesson_oop.py
@@ -1,13 +1,13 @@
 class Car:
     mileage: int = 0
-    fuel_consumption: float = 0.0
 
-    def __init__(self, brand: str, manufacturer: str, year: int = 2020):
+    def __init__(self, brand: str, manufacturer: str, year: int = 2020, fuel_consumption: float = 0.0):
         self.brand = brand.upper()
         self.manufacturer = manufacturer.title()
         self.year = year
+        self.fuel_consumption = fuel_consumption
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (
             f"Марка авто: {self.brand},\n"
             f"Виробник: {self.manufacturer},\n"
@@ -21,8 +21,8 @@ class Car:
         return "Beeeee! Beeeee! Beeeee!"
 
 
-vw = Car("volkswagen", "germany", year=2010)
-toyota = Car("tyOta", "Jpan", 2005)
+vw = Car("volkswagen", "germany", 2010)
+toyota = Car("tyOta", "Jpan", 2005, 8)
 bmw = Car("bmw", "germany")
 vw.mileage = 100_050
 vw.fuel_consumption = 8.5
